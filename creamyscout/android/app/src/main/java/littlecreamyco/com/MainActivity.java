b69aa4b978049b7816d6994d6581fdad6e459683
JacobXXI <jacobphhj@gmail.com>
2025-11-30 16:51:08 +1100

    Prepare for andriod deploy

---
+++ creamyscout/android/app/src/main/java/littlecreamyco/com/MainActivity.java
@@ -0,0 +1,5 @@
+package littlecreamyco.com;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ creamyscout/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'littlecreamyco.com',
+  appName: 'creamyscout',
+  webDir: 'dist'
+};
+
+export default config;
